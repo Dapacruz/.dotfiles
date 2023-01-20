0044726632618a056101778370e1fb26832d52fb
David Cruz <davidcruz72@gmail.com>
2023-01-19 21:51:34 -0800

    update script paths

--- macos/stow/bin/bin/tmux-cht.sh
+++ macos/stow/bin/bin/tmux-cht.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-selected=`cat ~/scripts/.tmux-cht-languages ~/scripts/.tmux-cht-command | fzf`
+selected=`cat ~/.local/bin/.tmux-cht-languages ~/.local/bin/.tmux-cht-command | fzf`
 if [[ -z $selected ]]; then
     exit 0
 fi
@@ -8,7 +8,7 @@ fi
 read -p "Enter Query: " query
 query=`echo $query | tr ' ' '+'`
 
-if grep -qs "$selected" ~/scripts/.tmux-cht-languages; then
+if grep -qs "$selected" ~/.local/bin/.tmux-cht-languages; then
     bash -c "echo \"http https://cht.sh/$selected/$query/\" & http https://cht.sh/$selected/$query | bat --paging always"
 else
     bash -c "echo \"http https://cht.sh/$selected~$query/\" & http https://cht.sh/$selected~$query | bat --paging always"
