c34424f292edf85d9d3e97830fa4db3feb9d19a3
David Cruz <davidcruz72@gmail.com>
2023-01-21 09:28:40 -0800

    update bfg alias

--- macos/stow/bin/bin/backup-awx.sh
+++ macos/stow/bin/bin/backup-awx.sh
@@ -2,4 +2,4 @@
 
 cd $HOME/Ansible/WSI/Local
 /usr/local/bin/ansible-playbook playbooks/awx/backup.yml > /tmp/backup-awx.log
-curl -X POST -H 'Content-type: application/json' --data "{'text':'Backup of AWX is now complete.'}" '***REMOVED***'
+curl -X POST -H 'Content-type: application/json' --data "{'text':'Backup of AWX is now complete.'} $WEBHOOK_WORK
