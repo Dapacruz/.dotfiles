46a5b1e885794a857bcc7ed51b901fce8d280d4d
David Cruz <davidcruz72@gmail.com>
2023-04-06 14:47:49 -0700

    update configs

---
+++ macos/stow/bin/bin/pass.sh
@@ -0,0 +1,10 @@
+defaults write org.p0deje.Maccy ignoreEvents true
+osascript -e "display notification \"Decrypting ${@} password ...\" with title \"Unix Pass\""
+#sleep 1
+result=$(/opt/homebrew/bin/pass -c "$@")
+if [[ $result == "" ]]; then
+    result="Insert and unlock your security key."
+fi
+osascript -e "display notification \"${result}\" with title \"Unix Pass\""
+sleep 1
+defaults write org.p0deje.Maccy ignoreEvents false
