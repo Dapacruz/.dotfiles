54929bab19bb2a96b3cd249786f458d5f2bed432
David Cruz <davidcruz72@gmail.com>
2023-05-18 13:03:35 -0700

    update configs

--- macos/stow/bin/bin/t.sh
+++ macos/stow/bin/bin/t.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 if [ "$1" = "-h" ]; then
     echo ""
@@ -13,9 +13,9 @@ if [ "$1" = "-h" ]; then
     echo ""
 elif [ $# -eq 0 ]; then
     if [ -z "$TMUX" ]; then
-        ZOXIDE_RESULT=$(zoxide query -l | fzf)
+        ZOXIDE_RESULT=$(echo "$HOME\n$(zoxide query -l)" | fzf)
     else
-        ZOXIDE_RESULT=$(zoxide query -l | fzf-tmux -p --reverse)
+        ZOXIDE_RESULT=$(echo "$HOME\n$(zoxide query -l)" | fzf-tmux -p --reverse)
     fi
 else
     ZOXIDE_RESULT=$(zoxide query $1)
