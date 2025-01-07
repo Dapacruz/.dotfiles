1bd0528c839ec7d391b1178e377f4ecd31dd80c0
dapacruz <davidcruz72@gmail.com>
2025-01-06 17:08:46 -0800

    Update Neovim config

--- macos/stow/nvim/nvim/lua/user/lsp/handlers.lua
+++ macos/stow/nvim/nvim/lua/user/lsp/handlers.lua
@@ -60,31 +60,35 @@ end
 
 local function lsp_keymaps(bufnr)
     local opts = { noremap = true, silent = true }
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "gI", "<Cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>", "<Cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<Cmd>lua vim.lsp.buf.references()<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<Cmd>lua vim.lsp.buf.rename()<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<Cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-    vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
-    vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
-    vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
-    vim.keymap.set("n", "<leader>wl", function()
+    local tabl_ext = vim.tbl_extend
+    local keymap_set_buf = vim.api.nvim_buf_set_keymap
+    local keymap_set = vim.keymap.set
+
+    keymap_set_buf(bufnr, "n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+    keymap_set_buf(bufnr, "n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+    keymap_set_buf(bufnr, "n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+    keymap_set_buf(bufnr, "n", "gI", "<Cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+    keymap_set_buf(bufnr, "n", "<C-k>", "<Cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+    keymap_set_buf(bufnr, "n", "gr", "<Cmd>lua vim.lsp.buf.references()<CR>", opts)
+    keymap_set_buf(bufnr, "n", "<leader>rn", "<Cmd>lua vim.lsp.buf.rename()<CR>", opts)
+    keymap_set_buf(bufnr, "n", "<leader>ca", "<Cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+    keymap_set("n", "<leader>D", vim.lsp.buf.type_definition, tabl_ext('force', opts, { desc = "Get LSP type definition" }))
+    keymap_set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+    keymap_set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+    keymap_set("n", "<leader>wl", function()
         print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
     end, opts)
-    vim.api.nvim_buf_set_keymap(
+    keymap_set_buf(
         bufnr,
         "n",
         "<leader><leader>d",
         '<Cmd>lua vim.diagnostic.open_float({ border="rounded" })<CR>',
         opts
     )
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", '<Cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", '<Cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>dll", "<Cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-    vim.keymap.set("n", "<space>di", function()
+    keymap_set_buf(bufnr, "n", "[d", '<Cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
+    keymap_set_buf(bufnr, "n", "]d", '<Cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
+    keymap_set_buf(bufnr, "n", "<leader>dll", "<Cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+    keymap_set("n", "<space>di", function()
         if vim.diagnostic.config().virtual_text then
             vim.diagnostic.config({ virtual_text = false })
         else
@@ -92,7 +96,7 @@ local function lsp_keymaps(bufnr)
         end
     end, opts)
     vim.cmd([[ command! Format execute "lua vim.lsp.buf.format({async=true})" ]])
-    vim.keymap.set("n", "<space>F", function()
+    keymap_set("n", "<space>F", function()
         vim.lsp.buf.format { async = true }
     end, vim.tbl_extend("force", opts, { desc = "LSP format current buffer" }))
 end
--- macos/stow/nvim/nvim/lua/user/plugins/harpoon.lua
+++ macos/stow/nvim/nvim/lua/user/plugins/harpoon.lua
@@ -7,16 +7,16 @@ return {
 
         harpoon:setup()
 
-        vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end)
-        vim.keymap.set("n", "<leader>he", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end, { desc = "Append to Harpoon list"})
+        vim.keymap.set("n", "<leader>he", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Toggle Harpoon quick menu"})
 
-        vim.keymap.set("n", "<A-7>", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<A-8>", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<A-9>", function() harpoon:list():select(3) end)
-        vim.keymap.set("n", "<A-0>", function() harpoon:list():select(4) end)
+        vim.keymap.set("n", "<A-7>", function() harpoon:list():select(1) end, { desc = "Select first Harpoon list entry"})
+        vim.keymap.set("n", "<A-8>", function() harpoon:list():select(2) end, { desc = "Select second Harpoon list entry"})
+        vim.keymap.set("n", "<A-9>", function() harpoon:list():select(3) end, { desc = "Select third Harpoon list entry"})
+        vim.keymap.set("n", "<A-0>", function() harpoon:list():select(4) end, { desc = "Select fourth Harpoon list entry"})
 
         -- Toggle previous & next buffers stored within Harpoon list
-        vim.keymap.set("n", "<leader>hp", function() harpoon:list():prev() end)
-        vim.keymap.set("n", "<leader>hn", function() harpoon:list():next() end)
+        vim.keymap.set("n", "<leader>hp", function() harpoon:list():prev() end, { desc = "Go to Harpoon previous list entry"})
+        vim.keymap.set("n", "<leader>hn", function() harpoon:list():next() end, { desc = "Go to Harpoon next list entry"})
     end,
 }
