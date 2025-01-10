bbd310a98426208480a4bc3b8fa2f22f2bec1901
dapacruz <davidcruz72@gmail.com>
2025-01-09 18:59:11 -0800

    Update script

--- macos/stow/bin/bin/fzf-bookmarks.sh
+++ macos/stow/bin/bin/fzf-bookmarks.sh
@@ -3,7 +3,7 @@
 cd "$HOME/Library/Application Support/Vivaldi/Default"
 bookmarks="./Bookmarks"
 
-bookmark=$($HOME/.local/bin/get-bookmarks.py | $HOME/.fzf/bin/fzf)
+bookmark=$($HOME/.local/bin/get-bookmarks.py | /opt/homebrew/bin/fzf)
 bookmark=$(python3 -c "print('$bookmark'.split('')[-1])")
 
 url=$(/opt/homebrew/bin/jq --color-output -r --arg BOOKMARK "$bookmark" '.roots.bookmark_bar.children[]? | select(.name==$BOOKMARK) | .url' $bookmarks)
