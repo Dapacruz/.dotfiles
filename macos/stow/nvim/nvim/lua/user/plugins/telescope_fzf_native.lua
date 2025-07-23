86022c8b716a345cce4d8db0afc5f00870c80eaf
David Cruz <dcruz@dc-infrastructures.shared>
2025-07-22 17:06:20 -0700

    Update Telescope FZF

--- macos/stow/nvim/nvim/lua/user/plugins/telescope_fzf_native.lua
+++ macos/stow/nvim/nvim/lua/user/plugins/telescope_fzf_native.lua
@@ -1,3 +1,3 @@
 return {
-    'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+    'nvim-telescope/telescope-fzf-native.nvim', build = 'make'
 }
