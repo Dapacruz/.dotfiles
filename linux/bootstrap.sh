748ace3d070a5443a05010f37ea1bbea1e118a22
David Cruz <davidcruz72@gmail.com>
2025-10-30 08:09:00 -0700

    Update Linux config

--- linux/bootstrap.sh
+++ linux/bootstrap.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 
+read -s -p "sudo password: " sudo_pass && echo
+echo $sudo_pass > become_pass.txt
+
+echo $sudo_pass | sudo -S apt update
+echo $sudo_pass | sudo -S apt install -y ansible curl git
+
 echo "Executing Ansible playbook ..."
-ansible-pull -K -U https://github.com/Dapacruz/.dotfiles linux/ansible/playbooks/deploy-dotfiles.yml
+# ANSIBLE_CONFIG=$HOME/.dotfiles/linux/ansible/ansible.cfg ansible-pull --become-password-file become_pass.txt -i $HOSTNAME, --limit=localhost,$HOSTNAME -U https://github.com/Dapacruz/.dotfiles linux/ansible/playbooks/deploy-dotfiles.yml
+ansible-pull --become-password-file become_pass.txt -i $HOSTNAME, --limit=localhost,$HOSTNAME -U https://github.com/Dapacruz/.dotfiles linux/ansible/playbooks/deploy-dotfiles.yml
+
+rm become_pass.txt
+
