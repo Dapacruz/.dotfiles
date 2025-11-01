b220dfa3f945eed442a91becefbf29097cac22bb
David Cruz <deepness.disclose707@mailer.me>
2025-11-01 10:11:55 -0700

    Update macOS config

--- macos/bootstrap.sh
+++ macos/bootstrap.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+# read -s -p "sudo password: " sudo_pass && echo
+# echo $sudo_pass > become_pass.txt
+
 if ! command -v brew &> /dev/null
 then
     echo "Installing Homebrew ..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Dapacruz/.dotfiles/master/macos/packages/homebrew-install.sh)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 if ! command -v ansible &> /dev/null
@@ -13,4 +17,6 @@ then
 fi
 
 echo "Executing Ansible playbook ..."
+# ansible-pull --become-password-file become_pass.txt -i $HOSTNAME, --limit=localhost,$HOSTNAME -U https://github.com/Dapacruz/.dotfiles macos/ansible/playbooks/deploy-dotfiles.yml
 ansible-pull -U https://github.com/Dapacruz/.dotfiles macos/ansible/playbooks/deploy-dotfiles.yml
+
