b2c507ca604258d3a00fa20e80fca62e49e26391
David Cruz <deepness.disclose707@mailer.me>
2025-11-01 10:11:55 -0700

    Update macOS config

--- macos/bootstrap.sh
+++ macos/bootstrap.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-# read -s -p "sudo password: " sudo_pass && echo
-# echo $sudo_pass > become_pass.txt
+read -s -p "sudo password: " sudo_pass && echo
+echo $sudo_pass > become_pass.txt
+
+echo $sudo_pass | sudo -S sh -c "echo '${USER} ALL=(root) NOPASSWD: ALL' > /etc/sudoers.d/homebrew-temp"
 
 if ! command -v brew &> /dev/null
 then
     echo "Installing Homebrew ..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    CI=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
@@ -17,6 +19,9 @@ then
 fi
 
 echo "Executing Ansible playbook ..."
-# ansible-pull --become-password-file become_pass.txt -i $HOSTNAME, --limit=localhost,$HOSTNAME -U https://github.com/Dapacruz/.dotfiles macos/ansible/playbooks/deploy-dotfiles.yml
-ansible-pull -U https://github.com/Dapacruz/.dotfiles macos/ansible/playbooks/deploy-dotfiles.yml
+ansible-pull --become-password-file become_pass.txt -i $HOSTNAME, --limit=localhost,$HOSTNAME -U https://github.com/Dapacruz/.dotfiles macos/ansible/playbooks/deploy-dotfiles.yml
+
+echo "Removing become_pass.txt and /etc/sudoers.d/homebrew-temp files ..."
+rm become_pass.txt
+echo $sudo_pass | sudo -S rm /etc/sudoers.d/homebrew-temp
 
