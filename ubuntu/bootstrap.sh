feb64baa81ae04c554a3d1bf7bdc54785f1e2b66
David Cruz <deepness.disclose707@mailer.me>
2026-04-22 09:35:16 -0700

    Rename linux to ubuntu

--- ubuntu/bootstrap.sh
+++ ubuntu/bootstrap.sh
@@ -7,7 +7,7 @@ echo $sudo_pass | sudo -S apt update
 echo $sudo_pass | sudo -S apt install -y ansible curl git stow
 
 echo "Executing Ansible playbook ..."
-ansible-pull --become-password-file become_pass.txt -i $HOSTNAME, --limit=localhost,$HOSTNAME -U https://github.com/Dapacruz/.dotfiles linux/ansible/playbooks/deploy-dotfiles.yml
+ansible-pull --become-password-file become_pass.txt -i $HOSTNAME, --limit=localhost,$HOSTNAME -U https://github.com/Dapacruz/.dotfiles ubuntu/ansible/playbooks/deploy-dotfiles.yml
 
 rm become_pass.txt
 
