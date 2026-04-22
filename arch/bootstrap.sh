5d88023627230c618a973b9964624b88146ebeb7
David Cruz <deepness.disclose707@mailer.me>
2026-04-22 10:26:45 -0700

    Add Arch playbook dependencies

--- arch/bootstrap.sh
+++ arch/bootstrap.sh
@@ -3,7 +3,7 @@
 read -s -p "sudo password: " sudo_pass && echo
 echo $sudo_pass > become_pass.txt
 
-echo $sudo_pass | sudo -S pacman -Syq --noconfirm ansible curl git stow
+echo $sudo_pass | sudo -S pacman -Syq --needed --noconfirm ansible curl git stow
 
 echo "Executing Ansible playbook ..."
 ansible-pull --become-password-file become_pass.txt -i $HOSTNAME, --limit=localhost,$HOSTNAME -U https://github.com/Dapacruz/.dotfiles arch/ansible/playbooks/deploy-dotfiles.yml
